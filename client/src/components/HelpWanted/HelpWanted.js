c205c60ddba8ad146e06037afe60dd74bbc3f4bb
David Bauer <bauer.david@att.net>
2018-03-31 11:29:50 -0400

    added folder structure to components

---
+++ client/src/components/HelpWanted/HelpWanted.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {  Label, Input } from 'reactstrap';
+import './HelpWanted.css';
+
+class HelpWanted extends Component {
+  render() {
+    return (
+      <div className="help-wanted">
+        <h1>Help Wanted</h1>
+        <Label for="exampleEmail">Input without validation</Label>
+        <Input />
+      </div>
+    );
+  }
+}
+
+export default HelpWanted;
