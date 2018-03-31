df3545cfbe3dfac84ecb3d5bf4f777e620612a0e
Angelina Yarichevskaya <angelina3yar@gmail.com>
2018-03-31 12:00:54 -0400

    added files

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const router = require('express').Router();
+const apiRoutes = require('./apiRoutes');
+const offersRoute = require('./offersRoute');
+const profileRoute = require('./profileRoute');
+const searchRoute = require('./searchRoute');
+
+router.use('/api', apiRoutes);
+router.use('/offers', offersRoute);
+router.use('/profile', profileRoute);
+router.use('/search', searchRoute);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/';
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Send every request to the React app
 // Define any API routes before this runs
