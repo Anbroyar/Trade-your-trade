a1ffb432835e23786dda2a26ce89721080c1ae8e
David Bauer <bauer.david@att.net>
2018-03-31 13:40:54 -0400

    beautified HelpWanted component

--- client/src/components/HelpWanted/HelpWanted.js
+++ client/src/components/HelpWanted/HelpWanted.js
@@ -7,38 +7,32 @@ class HelpWanted extends Component {
     return (
       <div className="help-wanted">
         <h1 className="header-text text-center">Help Wanted</h1>
-        <FormGroup row>
-        	<Col sm={2}></Col>
-	        <Col sm={2} className="column-one-text">Search for jobs within </Col>
-	        <Col sm={2}>
-	        	<Input type="select" name="select" id="exampleSelect">
-	            <option>10</option>
-	            <option>25</option>
-	            <option>50</option>
-	            <option>100</option>
-	          </Input>
-	        </Col>
-	        <Col sm={1} className="column-two-text"> miles of </Col>
-	        <Col sm={2}>
-	        	<Input name="zip" className="zip-input" placeholder="Enter Zipcode"/>
-	        </Col>
-	      </FormGroup>
-	      <FormGroup row>
-	        <Col sm={2} className="column-one-text">I'm looking for </Col>
-	        <Col sm={1}>
-	        	<Input name="hours" className="hours-input" placeholder="# hours"/>
-	        </Col>
-	        <Col sm={2} className="column-two-text"> hours of help with </Col>
-	        <Col sm={3}>
-	        	<Input type="select" name="skill" className="skill-input" placeholder="Enter Zipcode">
-	        		<option></option>
+        <Form inline className="form-search-radius">
+        	<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+		        <Label for="radiusSelect" className="label-text">Search for jobs within</Label>
+		        	<Input type="select" name="select" id="radiusSelect">
+		            <option>10</option>
+		            <option>25</option>
+		            <option>50</option>
+		            <option>100</option>
+		          </Input>
+		        <Label for="zipSelect" className="label-text">miles of</Label>
+		        <Input name="zip" id="zipSelect" className="zip-input" placeholder="Enter Zipcode"/>
+	        </FormGroup>
+	      </Form>
+	      <Form inline className="form-search-skills">
+	        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+	        	<Label for="hoursSelect" className="label-text">I'm looking for</Label>
+	        	<Input name="hours" id="hoursSelect" className="hours-input" placeholder="# hours"/>
+	        	<Label for="skillSelect" className="label-text">hours of work with</Label>
+	        	<Input type="select" name="skill" id="skillSelect" className="skill-input">
+	        		<option value="" selected disabled hidden>Select Skill</option>
+	        		<option>Carpentry</option>
+	        		<option>Electrical</option>
 	        	</Input>
-	        </Col>
-	        <Col sm={1}> work. </Col>
-	        <Col sm={2}>
-	        	<Button>Create Ad</Button>
-	        </Col>
-	      </FormGroup>
+	        	<Button className="create-ad">Create Ad</Button>
+	        </FormGroup>
+	      </Form>
       </div>
     );
   }
