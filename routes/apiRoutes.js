d508765ceec2eee5484135c139c2652818e221c4
David Bauer <bauer.david@att.net>
2018-04-03 20:16:16 -0400

    added mustBeLoggedIn.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const db = require('../models');
+const db = require('../../shared/models');
+const mustBeLoggedIn = require('../../shared/middleware/mustBeLoggedIn');
 
-function getUser(req, res) {
+function getCurrentUser(req, res) {
+  // I'm picking only the specific fields its OK for the audience to see publicly
+  // never send the whole user object in the response, and only show things it's OK
+  // for others to read (like ID, name, email address, etc.)
   const { id, username } = req.user;
   res.json({
     id, username
@@ -15,11 +19,11 @@ router.route('/auth')
   .get((req, res) => {
     if (!req.user) {
       return res.status(401).json({
-        message: 'You are not logged in.'
+        message: 'You are not currently logged in.'
       })
     }
 
-    getUser(req, res);
+    getCurrentUser(req, res);
   })
   // POST to /api/auth with username and password will authenticate the user
   .post(passport.authenticate('local'), (req, res) => {
@@ -29,7 +33,7 @@ router.route('/auth')
       })
     }
 
-    getUser(req, res);
+    getCurrentUser(req, res);
   })
   // DELETE to /api/auth will log the user out
   .delete((req, res) => {
@@ -51,17 +55,32 @@ router.route('/users')
         });
       })
       .catch(err => {
+        // if this error code is thrown, that means the username already exists.
+        // let's handle that nicely by redirecting them back to the create screen
+        // with that flash message
         if (err.code === 11000) {
           res.status(400).json({
             message: 'Username already in use.'
           })
         }
+
+        // otherwise, it's some nasty unexpected error, so we'll just send it off to
+        // to the next middleware to handle the error.
         next(err);
       });
   });
-router.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+
+// this route is just returns an array of strings if the user is logged in
+// to demonstrate that we can ensure a user must be logged in to use a route
+router.route('/stuff')
+  .get(mustBeLoggedIn(), (req, res) => {
+    // at this point we can assume the user is logged in. if not, the mustBeLoggedIn middleware would have caught it
+    res.json([
+      'Bears',
+      'Beets',
+      'Battlestar Galactica'
+    ]);
+  });
 
 
 module.exports = router;
