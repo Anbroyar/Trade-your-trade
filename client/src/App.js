7b0b14ea104e37d6f4b71a0e06fb69002ce5bb43
Mike DeFrank <mjdefrank@gmail.com>
2018-04-05 20:37:28 -0400

    created profiile route and inserted component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import HelpWanted from './components/HelpWanted';
 import Navbar from './components/Navbar';
+import Profile from './components/Profile/ProfilePage';
 
 class App extends Component {
   render() {
@@ -14,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Homepage} />
             <Route exact path="/helpwanted" component={HelpWanted} />
+            <Route exact path="/profile" component={Profile} />
           </Switch>
           </Fragment> 
         </Router>
