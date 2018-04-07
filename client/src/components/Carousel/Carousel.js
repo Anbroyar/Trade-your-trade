4d878b48ea064e436cfefb3262a38d6e4aedba0e
Angelina Yarichevskaya <angelina3yar@gmail.com>
2018-04-07 12:48:38 -0400

    fixed carousel

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -5,15 +5,14 @@ import BACKGROUND from '../../images/BACKGROUND.jpg';
 import coder from '../../images/coder.jpg';
 import image106 from '../../images/image106.jpg';
 import Deals from '../../images/Deals.jpg';
-import contractors from '../../images/contractors.jpg';
 import bkp504 from '../../images/bkp504.jpg';
 import workers from '../../images/workers.jpg';
 import accountant from '../../images/accountant.jpg';
 const items = [
   {
     src: BACKGROUND,
-    altText: 'What We Do',
-    caption: 'We provide skilled laborers with a platform to exchange services. People can negotiate the hourly value of their specific trade, in exchange for the comparable value of someone elses.Trade your trade is the best of capitalism, and the best of communism.'
+    altText: 'We provide skilled laborers with a platform to exchange services. People can negotiate the hourly value of their specific trade, in exchange for the comparable value of someone elses.Trade your trade is the best of capitalism, and the best of communism.',
+    caption: ''
   },
   { 
     src: coder,
@@ -27,11 +26,6 @@ const items = [
   },
   {
     src: Deals,
-    altText: '',
-    caption: ''
-  },
-  {
-    src: contractors,
     altText: '',
     caption: ''
   },
