a21869df8d2b5605724b26afa4e5a28cabe489da
Angelina Yarichevskaya <angelina3yar@gmail.com>
2018-04-10 19:50:12 -0400

    added seeds

--- models/skills_model.js
+++ models/skills_model.js
@@ -14,6 +14,23 @@ Skills.seeds = () => {
   Skills.create([
     {name: 'DJ'},
     {name: 'Carpenter'},
-    {name: 'The Dude'}
+    {name: 'Developer'},
+    {name: 'Designer'},
+    {name: 'Babysiter'},
+    {name: 'Accountant'},
+    {name: 'Architect'},
+    {name: 'Photographer'},
+    {name: 'Plumber'},
+    {name: 'Landscaper'},
+    {name: 'Musician'},
+    {name: 'Cook'},
+    {name: 'Hairdresser'},
+    {name: 'MusicTeacher'},
+    {name: 'Mechanic'},
+    {name: 'Nutritionist'},
+    {name: 'PetGroomer'},
+    {name: 'Roofer'},
+    {name: 'Tutor'},
+    {name: 'Housekeeper'},
   ])
 };
