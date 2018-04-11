447cf67f41ea5e74a60727cccb971d7bcc2f2d2c
tanickel1 <tanickel1@gmail.com>
2018-04-10 21:18:40 -0400

    drop"n seeds

--- models/connection.js
+++ models/connection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/trade_your_trade';
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+const connection = mongoose.connect(MONGODB_URI);
+module.exports = connection;
+connection.DELETE = () => mongoose.connection.dropDatabase()
--- models/seed.js
+++ models/seed.js
@@ -1,18 +1,22 @@
 const Skills = require('./skills_model');
 const Users = require('./user_model');
 const Jobs = require('./jobs_model');
-require('./connection');
+const connection = require('./connection');
 
-Skills
-  .seeds()
-  .then(skills => {
-    return Users.seeds(skills)
-      .then(users => {
-        return Jobs.seeds(users, skills)
+connection.DELETE()
+  .then(() =>
+    Skills
+      .seeds()
+      .then(skills => {
+        return Users.seeds(skills)
+          .then(users => {
+            return Jobs.seeds(users, skills)
+          })
       })
-  })
-  .then(() => console.log('Done seeding!'))
-  .catch((error) => {
-    console.log('Error seeding!', error);
-  })
-  .then(() => process.exit(0))
+      .then(() => console.log('Done seeding!'))
+      .catch((error) => {
+        console.log('Error seeding!', error);
+        process.exit(1);
+      })
+      .then(() => process.exit(0))
+)
